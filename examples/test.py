644c0c57c0eabb7d99ce0bf7bf5b751649965512
Akshath Kachinthaya <akachint@blip.ece.local.cmu.edu>
2024-03-20 13:33:54 -0400

    Visulaizer updates post dash app experiments

--- examples/test.py
+++ examples/test.py
@@ -1,5 +1,5 @@
 
-from schematic import schematic
+from pytessent import schematic
 
 # create "tessent -shell" process and write log file to tessent.log
 log_file_name = "tessent.log"
@@ -14,6 +14,7 @@ schematic.readFlatModel(pt_shell, flat_model_path)
 
 # Pins to display
 pins = [ "s1196_i_g5717__9945/Y", "s1196_i_g5781__8246/Y", "G4_flop_i_qi_reg/Q", ]
+#['g10194/Y', 'g10158__7098/Y', 'g10208__1617/Y']
 
 # Display pins 
 schematic.displayPins(pt_shell, pins)
--- src/pytessent/schematic.py
+++ src/pytessent/schematic.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from pytessent import PyTessent
 
 class schematic:
-    """Class for tessent flat schematic visulaizer functions"""
+    """Class for tessent flat schematic visualizer functions"""
 
     # Function to create "tessent -shell" process and write log file to logfile argument
     def __init__ (self, logfileName : str):
@@ -10,18 +10,31 @@ class schematic:
         self.shell = PyTessent.launch(timeout=None, logfile=logfilePath, replace=1)
 
     # Function to display list of pins on the flat schematic 
-    def displayPins (self, pins, message: str = "Component", levels: int = 0):
+    def displayPins (self, pin_dict: dict, message: str = "Component", levels: int = 1, enEP: bool = False,
+                     voxelColor: str = "pink", fwdColor: str = "green", bwdColor: str = "blue", 
+                     ipinColor: str = "red", iopinColor: str = "orange", opinColor: str = "yellow"):
+        pins  = pin_dict.get('pinPath'  , [])
+        ipins = pin_dict.get('I_pinpath', [])
+        opins = pin_dict.get('O_pinpath', [])
+        io_pins = list(set(ipins).intersection(set(opins)))
         pinList = '{{{}}}'.format(' '.join(map(str, pins)))
-        result1 = self.shell.sendCommand(f"add_schematic_objects     {pinList} -highlight red")
-        result2 = self.shell.sendCommand(f"add_schematic_callout     {pinList} -message \"{message}\"")
-        if levels > 0:
-            result3 = self.shell.sendCommand(f"add_schematic_connections {pinList} -forward  decision_point -distance_limit {levels} -highlight pink")
-            result4 = self.shell.sendCommand(f"add_schematic_connections {pinList} -backward decision_point -distance_limit {levels} -highlight blue")
-        else:
-            result3 = ""
-            result4 = ""
-        result5 = self.shell.sendCommand(f"add_schematic_connections {pinList} -to {pinList} -distance_limit 3 -highlight green")
-        print (result1 + result2 + result3 + result4 + result5)
+        ipinList = '{{{}}}'.format(' '.join(map(str, ipins)))
+        opinList = '{{{}}}'.format(' '.join(map(str, opins)))
+        iopinList = '{{{}}}'.format(' '.join(map(str, io_pins)))
+        result = "\nDisplaying Pins: \n"
+        result += self.shell.sendCommand(f"add_schematic_objects     {pinList} -highlight {voxelColor}")
+        result += self.shell.sendCommand(f"add_schematic_callout     {pinList} -message \"{message}\"")
+        if enEP:
+            result += self.shell.sendCommand(f"add_schematic_connections {pinList} -forward  end_point      -distance_limit {levels} -highlight {fwdColor}")
+            result += self.shell.sendCommand(f"add_schematic_connections {pinList} -backward end_point      -distance_limit {levels} -highlight {bwdColor}")
+        result += self.shell.sendCommand(f"add_schematic_connections {pinList} -forward  decision_point -distance_limit {levels} -highlight {fwdColor}")
+        result += self.shell.sendCommand(f"add_schematic_connections {pinList} -backward decision_point -distance_limit {levels} -highlight {bwdColor}")
+        result += self.shell.sendCommand(f"add_schematic_objects     {ipinList} -highlight {ipinColor}")
+        result += self.shell.sendCommand(f"add_schematic_objects     {opinList} -highlight {opinColor}")
+        result += self.shell.sendCommand(f"add_schematic_objects     {iopinList}  -highlight {iopinColor}")
+        # result += self.shell.sendCommand(f"add_schematic_connections {pinList} -to {pinList} -distance_limit 1 -highlight {voxelColor}")
+        if (result != "\nDisplaying Pins: \n"):
+            print (f"{result}\n")
 
     # Clear all schematic objects
     def clearSchm (self):
@@ -36,7 +49,6 @@ class schematic:
         return self.shell.sendCommand(command)
 
     # Read Tessent flat model
-    # TODO: If flat model is corrupted, raise exception
     def readFlatModel (self, flat_model_path : str):
         flatModelPath = Path(flat_model_path)
         result = self.shell.sendCommand(f"read_flat_model {flatModelPath}")
@@ -51,6 +63,7 @@ class schematic:
     # Read delay data from sdf file
     def readSDFfile (self, sdf_file_path : str):
         sdfFilePath = Path(sdf_file_path)
+        self.shell.sendCommand("set_fault_type transition")
         result = self.shell.sendCommand(f"read_sdf {sdfFilePath}")
         print (result)
 
